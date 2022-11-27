f56659442699c6fc86e238f5d963015f68f43f87
Tomasz Greber <grebciu@gmail.com>
2022-11-27 08:59:50 +0100

    01:20:00

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length = 200)
+    description = models.TextField(blank = True, null = True)
+    # participants = 
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Home')
+rooms = [
+    {
+        'id': 1,
+        'name': 'Room #1',
+    },
+    {
+        'id': 2,
+        'name': 'Room #2',
+    },
+    {
+        'id': 3,
+        'name': 'Room #3',
+    },
+]
 
-def room(request):
-    return HttpResponse('Room')
+def home(request):
+    context = {
+        'rooms': rooms,
+    }
+
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    
+    context = {
+        'room': room
+    }
+
+    return render(request, 'base/room.html', context)
